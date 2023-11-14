61c097a207ddebc6794d4b0dcea8f3b3ac04390d
Daniil-Solo <daniil.solo1723@gmail.com>
2023-11-14 16:27:15 +0500

    add config

---
+++ src/config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    """Настройки для сервиса"""
+
+    token: str = Field(alias="TOKEN")
+    n_returned_items: int = Field(alias="N_RETURNED_ITEMS", default=10)
+
+
+settings = Settings()
