853126f9ab22cd040484142ea8e87e1bf7adfa5d
Daniil-Solo <daniil.solo1723@gmail.com>
2023-11-14 16:32:34 +0500

    add app

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from src.api.router import router
+from src.exceptions import RecSysServiceError
+
+app = FastAPI(
+    title="Сервис рекомендательной системы",
+    version="0.0.1",
+    docs_url="/docs",
+    redoc_url="/docs/redoc",
+)
+
+app.include_router(router)
+
+
+@app.exception_handler(RecSysServiceError)
+async def handle_rec_sys_service_errors(_: Request, exc: RecSysServiceError):
+    """
+    Обрабатывает ошибки сервиса
+    """
+    return JSONResponse(
+        content={"message": exc.error_message}, status_code=exc.status_code
+    )
