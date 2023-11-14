6abb2e6f4510872e85f5ee03ab2bf2cb6b26db47
Daniil-Solo <daniil.solo1723@gmail.com>
2023-11-14 17:14:19 +0500

    add default value of token for tests

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Settings(BaseSettings):
     """Настройки для сервиса"""
 
-    token: str = Field(alias="TOKEN")
+    token: str = Field(alias="TOKEN", default="TEST_TOKEN")
     n_returned_items: int = Field(alias="N_RETURNED_ITEMS", default=10)
 
 
