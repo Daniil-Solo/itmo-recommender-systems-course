16a369c1c7a801772c81ab9803a465c7c4c723cb
Daniil-Solo <daniil.solo1723@gmail.com>
2023-11-21 14:43:36 +0300

    tranlate comments in app, config to english

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from src.api.router import router
 from src.exceptions import RecSysServiceError
 
 app = FastAPI(
-    title="Сервис рекомендательной системы",
+    title="Recommendation System Service",
     version="0.0.1",
     docs_url="/docs",
     redoc_url="/docs/redoc",
@@ -17,7 +17,7 @@ app.include_router(router)
 @app.exception_handler(RecSysServiceError)
 async def handle_rec_sys_service_errors(_: Request, exc: RecSysServiceError):
     """
-    Обрабатывает ошибки сервиса
+    Handles service errors
     """
     return JSONResponse(
         content={"message": exc.error_message}, status_code=exc.status_code
