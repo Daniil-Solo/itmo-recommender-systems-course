58a93343cd7f1b39a637cdfae244d2ce80b7b605
Daniil-Solo <daniil.solo1723@gmail.com>
2023-12-06 16:21:57 +0300

    add loading model on startup application

--- src/api/router.py
+++ src/api/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Header, Path
+from fastapi import APIRouter, Depends, Header, Path, Request
 
 from src.api.auth import check_authorization_token
 from src.api.dependencies import get_user_id
@@ -27,6 +27,7 @@ async def health_check():
     responses={401: {"model": ErrorMessage}, 404: {"model": ErrorMessage}},
 )
 async def get_recommendations(
+    request: Request,
     model_name: str = Path(),
     user_id: int = Depends(get_user_id),
     authorization: str = Header(None),
@@ -36,6 +37,9 @@ async def get_recommendations(
     'model_name' for a user with id 'user_id'
     """
     check_authorization_token(authorization)
-    recommender = RecommenderRegister.get_recommender_by_model_name(model_name)
+    if request.app.state.recommender and request.app.state.recommender.MODEL_NAME == model_name:
+        recommender = request.app.state.recommender
+    else:
+        recommender = RecommenderRegister.get_recommender_by_model_name(model_name)
     items = recommender.predict(user_id)
     return UserRecommendationResponse(user_id=user_id, items=items)
--- src/app.py
+++ src/app.py
@@ -1,10 +1,25 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 from src.api.router import router
 from src.exceptions import RecSysServiceError
+from src.api.dependencies import get_actual_recommender
+from src.config import settings, ModeEnum
+
+
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+    """
+    Executes start actions
+    """
+    application.state.recommender = get_actual_recommender()
+    yield
+
 
 app = FastAPI(
+    lifespan=lifespan if settings.mode == ModeEnum.PRODUCTION else None,
     title="Recommendation System Service",
     version="0.0.1",
     docs_url="/docs",
@@ -19,6 +34,4 @@ async def handle_rec_sys_service_errors(_: Request, exc: RecSysServiceError):
     """
     Handles service errors
     """
-    return JSONResponse(
-        content={"message": exc.error_message}, status_code=exc.status_code
-    )
+    return JSONResponse(content={"message": exc.error_message}, status_code=exc.status_code)
