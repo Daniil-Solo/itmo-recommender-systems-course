1b75411e3997854c979d1ee854029adf5cf08d6a
Daniil-Solo <daniil.solo1723@gmail.com>
2023-12-13 12:03:59 +0300

    add getting recommender

--- src/config.py
+++ src/config.py
@@ -1,15 +1,25 @@
+from enum import Enum
 from dotenv import load_dotenv
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
-load_dotenv()
+
+class ModeEnum(str, Enum):
+    """
+    Mode of application
+    """
+
+    TEST = "test"
+    PRODUCTION = "prod"
 
 
 class Settings(BaseSettings):
     """Settings for the service"""
 
+    mode: ModeEnum = Field(alias="MODE", default=ModeEnum.TEST)
     token: str = Field(alias="TOKEN", default="TEST_TOKEN")
     n_returned_items: int = Field(alias="N_RETURNED_ITEMS", default=10)
 
 
+load_dotenv()
 settings = Settings()
